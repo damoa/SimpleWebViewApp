3386cbf466f29d46148ba22024d307fa7db3c100
Damoa <damoa.obiri.alcorta@sofatutor.com>
2024-10-04 10:37:48 +0200

    first commit

---
+++ app/src/main/java/com/example/rbbwetter/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.rbbwetter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.rbb24.de/wetter/wetterkarte/")
+    }
+}
